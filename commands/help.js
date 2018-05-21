a638e3281e340c708f87766e1768df7b8a09fa64
Kieran Coldron <kieran@coldron.com>
2018-05-21 19:46:33 +0100

    Add embeds and make help a code block (#1)
    * add embed to mentions for channels
    
    * add missing comma
    
    * debugging
    
    * fix color
    
    * add dm embed
    
    * code block for help command
    
    * more formatting for help
    
    * removed spacing
    
    * remove debugging line

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,13 @@
 module.exports = (message) => {
   return `
-commands
---------
-\`@mentionbot mode <channel|dm|off>\` : turn mentions on or off for your user and choose method
-\`@mentionbot name <username|nickname|both|off>\` : turn on mentions for your names
-\`@mentionbot add <1>, <2>, <3>\` : add phrases that you should be mentioned for
-\`@mentionbot remove <1>, <2>, <3>\` : remove phrases that you should be mentioned for
-\`@mentionbot list\` : list your settings and added mention phrases
-\`@mentionbot channel #<channel name>\` : set channel for mentions to appear in (admin only)
-\`@mentionbot help\` : display available commands
-  `
+Commands
+\`\`\`
+@mentionbot mode <channel|dm|off>: turn mentions on or off for your user and choose method
+@mentionbot name <username|nickname|both|off>: turn on mentions for your names
+@mentionbot add <1>, <2>, <3>: add phrases that you should be mentioned for
+@mentionbot remove <1>, <2>, <3>: remove phrases that you should be mentioned for
+@mentionbot list: list your settings and added mention phrases
+@mentionbot channel #<channel name>: set channel for mentions to appear in (admin only)
+@mentionbot help: display available commands\`\`\`
+`
 }
